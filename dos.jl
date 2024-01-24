c9b69f91f093b549eee03d98829ec584a34e587b
jacobmacwilliams <jacobmacwilliams@gmail.com>
2024-01-24 14:00:22 +0100

    Undo adding chemical potential in dos.jl

--- dos.jl
+++ dos.jl
@@ -6,6 +6,20 @@ include("setup.jl")
 
 Operators.addchemicalpotential!(H, -mu)
 frequencies, dos = Spectrum.getdos(H, -T / 2, T / 2, num_bins; Γ=gamma, klin=klin, format=:sparse, num_bands=num_bands)
+
+# This operation has to be undone here be spinmap.jl
+# expects the chemical potential hasn't been
+# embedded within the hamiltonian. This behaviour can't
+# be changed without changing LatticeQM because
+# densitymatrix_distributed is called in spinmap and
+# it is this function that ultimately makes this
+# assumption. Another work around would be to add the
+# chemical potential and then have spinmap assume the
+# chemical potential is zero. But that manipulates state
+# throughout the length of the program and I prefer this
+# because it doesn't.
+Operators.addchemicalpotential!(H, mu)
+
 plt = plot(title="Density of States")
 bar!(frequencies, dos)
 xlabel!("Energy")
