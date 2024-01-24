cebc4f5b65d33c6a0b7aff7a5bdab362e07b7678
jacobmacwilliams <jacobmacwilliams@gmail.com>
2024-01-24 13:01:43 +0100

    Beginning breaking apart ferrohubbard into smaller components.

---
+++ setup.jl
@@ -0,0 +1,40 @@
+using Distributed
+using JLD2
+using Plots
+
+const N = nworkers()
+if !(N > 1)
+	error("This code must be run distributed.")
+end
+
+@everywhere using LatticeQM
+@everywhere using LQMRunner
+@everywhere import LinearAlgebra.BLAS
+@everywhere BLAS.set_num_threads(1)
+
+const MODELNAME = ARGS[1]
+
+@info "Loading model " * MODELNAME * "..."
+const OUTPUTDIR = joinpath("output", MODELNAME * "_ana")
+if !isdir(OUTPUTDIR)
+	mkpath(OUTPUTDIR)
+end
+
+#const PROJECTROOT = pkgdir(LQMRunner)
+const MODELPATH = joinpath(pkgdir(LQMRunner), "models", MODELNAME * ".jld2")
+if !isfile(MODELPATH)
+	error("File not found")
+end
+
+
+const model = load(MODELPATH)["model"];
+const lat = getlattice(model)
+const H = gethamiltonian(model)
+const klin = getparams(model)[:klin]
+const ks = Structure.regulargrid(;nk=klin^2)
+#const T = getparams(model)[:T]
+#const doping = getparams(model)[:doping]
+const doping = -6
+
+@info "Calculating chemical potential at " * string(doping) * " charge doping..."
+const mu = getchemicalpotential(model, doping, ks; multimode=:distributed)
---
+++ spinmap.jl
@@ -0,0 +1,71 @@
+include("setup.jl")
+using ProgressMeter
+
+@everywhere function localspin_distributed(ρ, sz, sites::Vector{Int}, latsize::Int, results::RemoteChannel, pool::RemoteChannel)
+	spinzs = []
+	for i in sites
+		proj_i = kron(Diagonal([j == i for j in 1:latsize]), Matrix(1.0I, 2, 2))
+		spinz = real(Operators.trace(ρ, sz * proj_i))
+		push!(spinzs, spinz)
+	end
+	put!(results, (sites, spinzs))
+	put!(pool, myid())
+end
+
+@info "Calculating the spatially resolved magnetization of the ground state..."
+sitenumber = Lattices.countorbitals(lat)
+basis = Lattices.getA(lat)
+latpos = Lattices.positions(lat)
+
+# we will be translating the spin map by bravais vectors to see the emergent spin
+# texture on the full lattice.
+translations = [zeros(3), basis[:, 1], basis[:, 2], basis[:, 2] - basis[:, 1]]
+latpos = hcat([latpos .+ t for t in translations]...)
+
+# Recalculating density matrix from Hamiltonian
+ρ = getdensitymatrix(model, mu, ks; multimode=:distributed)
+sz = Operators.spin(lat, "sz")
+
+# Parsing latticeidx chunks for scheduling
+chunksize = ceil(sitenumber / N)
+sites = [i for i in 1:sitenumber]
+chunks = []
+for i in 1:N
+	startidx = Int(chunksize * (i - 1) + 1)
+	endidx = Int(chunksize * i)
+	if endidx > sitenumber
+		endidx = sitenumber
+	end
+	push!(chunks, sites[startidx:endidx])
+end
+
+# asynchronously schedule matrix multiplication on the workers and greedily scatter
+# results as as soon as availabe in the results channel. The worker pool guarantees that
+# the individual workers aren't "overscheduled/overworked".
+results = RemoteChannel()
+pool  = RemoteChannel(() -> Channel{Int}(N))
+for i in workers()
+	put!(pool, i)
+end
+
+pltb = plot()
+@sync begin
+	# Greedy processing
+	@async @showprogress "Plotting spin map..." for _ in eachindex(chunks)
+		siteidxs, spins = take!(results)
+		# copying results for translated unit cells
+		siteidxs = vcat([siteidxs .+ (i - 1) * sitenumber for i in eachindex(translations)]...)
+		spins = vcat([spins for i in eachindex(translations)]...)
+		scatter!(latpos[1,siteidxs], latpos[2, siteidxs]; markersize=2, zcolor=spins, color=:roma)
+	end
+	
+	# Scheduling
+	for chunk in chunks
+		worker = take!(pool)
+		remote_do(localspin_distributed, worker, ρ, sz, chunk, sitenumber, results, pool)
+	end
+end
+close(results)
+
+fname = joinpath(OUTPUTDIR, "spinmap.png")
+savefig(pltb, fname)
