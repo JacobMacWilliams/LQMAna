22552ff2dc46958db595244ab6589c6f0a8e44b7
jacobmacwilliams <jacobmacwilliams@gmail.com>
2024-01-24 13:27:58 +0100

    Add dos.jl

---
+++ dos.jl
@@ -0,0 +1,16 @@
+include("setup.jl")
+@info "Calculating the dos..."
+#avgspacing = (e2 - e1) * tps / tp / klin^2 / num_bands / 5
+
+num_bins = 100
+gamma = 0.0005
+#gamma = avgspacing / 2 / sqrt(exp(1)-1)
+
+Operators.addchemicalpotential!(H, -mu)
+frequencies, dos = Spectrum.getdos(H .- mu, -T / 2, T / 2, 100; Γ=gamma, klin=klin, format=:sparse, num_bands=num_bands)
+plt = plot(title="Density of States")
+bar!(frequencies, dos)
+xlabel!("Energy")
+ylabel!("DOS")
+fname = "DOS.png"
+savefig(plt, joinpath(OUTPUTDIR, fname))
